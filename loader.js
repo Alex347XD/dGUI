74dcbcc85b7a8b5312423276fd8b10c6c374000a
Alex347XD <zacharyv1001@gmail.com>
2026-06-23 17:44:55 -0600

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,4 @@
 // Load the Delta Math Cheat GUI
 (async () => {
-	eval(await (await fetch("https://raw.githubusercontent.com/DxltaMath/dGUI/master/dist/bundle.js")).text());
+	eval(await (await fetch("https://raw.githubusercontent.com/Alex347XD/dGUI/master/dist/bundle.js")).text());
 })();
